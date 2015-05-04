7241ba6e667ed29ac43f6199fe31b0019486b15a
ccy016 <ccy016@outlook.com>
2015-05-04 23:38:30 +0800

    test

--- src/com/drawguess/base/Constant.java
+++ src/com/drawguess/base/Constant.java
@@ -8,5 +8,16 @@ public class Constant {
     public static final int BUFFER_SIZE = 1024;// 流缓冲大小
     public static final int TCP_PORT = 4447; // 主机接收端口 
 	public static final int UDP_PORT = 3647; // 主机接收端口
+	
+	// Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
 }
 
